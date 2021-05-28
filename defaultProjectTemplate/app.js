55b389a065bc03d5284d04e21f772dbe7a02283f
onurcanalpp <onurcanalpp@gmail.com>
2021-05-28 18:22:20 +0300

    .

---
+++ defaultProjectTemplate/app.js
@@ -0,0 +1,30 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navlinks = document.querySelectorAll(".nav-links li");
+    
+    
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+        // Animate Links
+        navlinks.forEach((link, index) => {
+            if(link.style.animation) {
+                link.style.animation = '';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
+            }
+        });
+        // Burger Animation
+        burger.classList.toggle('toggle');
+
+    });
+    
+
+}
+
+const app = () => {
+    navSlide(); // Mobile Burger Menu Function
+}
+
+app();
