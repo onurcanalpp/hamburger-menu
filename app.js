e7dfb5d4ca49c12d0238a078528af0b69ce89e43
onurcanalpp <onurcanalpp@gmail.com>
2021-05-30 16:10:47 +0300

    burger mobile style changed and for navslide function. I added the new commits - and readme file edited.

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const navSlide = () => {
             if(link.style.animation) {
                 link.style.animation = '';
             }else{
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`; // animation calculating here 
             }
         });
         // Burger Animation
